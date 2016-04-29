23910287a69513a0dbca0b742323c296115f878f
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-04-29 21:39:23 +0200

    Cleanup

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@
 
 var config = {
   FIREBASE_URL: process.env.FIREBASE_URL || 'tessel2climate.firebaseio-demo.com',
-  LOOP_TIMEOUT: process.env.LOOP_TIMEOUT || 60000,
+  LOOP_TIMEOUT: process.env.LOOP_TIMEOUT || 600000,
   TEMPERATURE_FORMAT: process.env.TEMPERATURE_FORMAT || 'c'
 }
 
